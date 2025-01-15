4f88263036a3540d9554c77f716ba0ec8820c2ff
danish12-cyber <sdanish1472@gmail.com>
2025-01-15 09:36:00 +0000

    update

--- Views/Event.py
+++ Views/Event.py
@@ -5,17 +5,6 @@ st.title("Event Extractor")
 Url = st.text_input("Enter the Url", value="", max_chars=100)
 result=[]
 
-st.markdown(
-    """
-    <style>
-    [stMainBlockContainer]{
-        background-color: #f0f4f0;  # Set your desired background color
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 
 if st.button("Extract Event"):
     result= pl.event_extract(Url, 4)
